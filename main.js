d8e0f84f9ca14d0641fa310504105979d9554088
Anmol Rajbhandari <anmolrajbhandari12@gmail.com>
2025-10-25 23:11:45 +0545

    Implement Scrolling Functionality

---
+++ main.js
@@ -0,0 +1,15 @@
+const gallery = document.getElementById("gallery");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+
+gallery.addEventListener('wheel',(e)=>{
+    e.preventDefault();
+    gallery.scrollLeft += e.deltaY;
+})
+
+left.addEventListener('click',()=>{
+    gallery.scrollLeft += -300;
+})
+right.addEventListener('click',()=>{
+    gallery.scrollLeft  += 300;
+})
